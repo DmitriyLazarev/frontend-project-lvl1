c1e92cc46684d9932fb0a403d03b2cfc9f3c42c7
Dmitry Lazarev <dmitry.lazarev@team.wrike.com>
2022-02-08 14:32:24 +0100

    separate logic for brain-prime

--- src/games/brain-prime/brain-prime.js
+++ src/games/brain-prime/brain-prime.js
@@ -1,23 +1,23 @@
 import { gameCommon } from '../../common/modules/game-base.js';
 import { getRandomNumber } from '../../common/modules/get-random-number.js';
 
+const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
 const getPrimeNumberGameQuestionAndAnswer = () => {
   const question = getRandomNumber();
-  let expectedAnswer = 'yes';
-
-  if (question === 2 && question === 1) {
-    return [question, expectedAnswer];
-  }
-
-  let i = 2;
-  const limit = Math.sqrt(question);
-  while (i <= limit) {
-    if (question % i === 0) {
-      expectedAnswer = 'no';
-    }
-    i += 1;
-  }
-
+  const expectedAnswer = isPrime(question) ? 'yes' : 'no';
   return [question, expectedAnswer];
 };
 
